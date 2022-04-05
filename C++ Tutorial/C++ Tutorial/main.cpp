00ae4ac9940dcacffa71c078555439d24f0baebf
mikewang0520 <85894910+mikewang0520@users.noreply.github.com>
2022-04-04 23:10:31 -0500

    Add files via upload

---
+++ C++ Tutorial/C++ Tutorial/main.cpp
@@ -0,0 +1,122 @@
+//
+//  main.cpp
+//  C++ Tutorial
+//
+//  Created by Mike汪 on 2022/4/3.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    //1. String
+    //    char str[] = "Hello";
+    //    cout << str << endl;
+    //    string str = "Helloo";
+    //    cout << str << endl;
+    
+    //2. User input (cin)
+    //    int a = 0;
+    //    cout << "give a value of: " << endl;
+    //    cin >> a;
+    //    cout << "a is equal to: " << a <<  endl;
+    
+    //3. Switch
+    //    Switch(detemine variable)
+    //    {
+    //    case 1:
+    //        break;
+    //    case 2:
+    //        break;
+    //    default:
+    //        break;
+    //    }
+    
+    //4. Do..while
+    //do{}while() will excute once first compared to while(){}
+    
+    //5. For inside for
+//    for (int i = 0; i < 10; i++)
+//    {
+//        for (int j = 0; j < 10; j++)
+//        {
+//            cout << "* ";
+//        }
+//        cout << endl;
+//    }
+    
+    //6. Break & Continue
+    //break是跳出循环，continue是忽略它下面的代码重新一轮循环
+    
+    //7. Goto (not recmanded)
+//    goto FLAG;
+//    1;
+//    2;
+//    FLAG;
+//    3;
+    //jump to excute 3
+    
+    //8. Array
+//    int arr1[5];
+//    int arr2[5] = {10, 20, 30, 40, 50};
+//    int arr3[] = {0, 1, 2, 3};
+//    sizeof(arr1);
+//    sizeof(arr1[0]);
+//    number of elements = sizeof(arr1) / sizeof(arr1[0]);
+//    address of array = arr;
+//    address of element = &arr[i];
+    
+    //9. Bubble sorting
+//    int arr[] = {4, 2, 8, 0, 5, 7, 1, 3, 9};
+//    cout << "Before: " << endl;
+//    for (int i = 0; i < 9; i++)
+//    {
+//        cout << arr[i] << " ";
+//    }
+//    cout << endl;
+//
+//    for (int i = 0; i < 9 - 1; i++)
+//    {
+//        for (int j = 0; j < 9 - i - 1; j++)
+//        {
+//            if (arr[j] > arr[j + 1])
+//            {
+//                int temp = arr[j];
+//                arr[j] = arr[j + 1];
+//                arr[j + 1] = temp;
+//            }
+//        }
+//    }
+//
+//    cout << "After: " << endl;
+//    for (int i = 0; i < 9; i++)
+//    {
+//        cout << arr[i] << " ";
+//    }
+//    cout << endl;
+ 
+    //10. 乘法口诀表
+//    for(int i = 1; i <= 9; i++)
+//    {
+//        for (int j = 1; j <= i; j++)
+//        {
+//            cout << j << "*" << i << "=" << j*i << " ";
+//        }
+//        cout << endl;
+//    }
+    
+    //11. 2D array
+//    int arr[num of raw][num of cloum];
+//    float arr[2][3] = {1.1, 1.2, 1.3, 2.1, 2.2, 2.3};
+//    float arr[2][3] =
+//    {
+//        {1.1, 1.2, 1.3},
+//        {2.1, 2.2, 2.3}
+//    };
+    
+    //12.
+    
+    //system("pause");
+    return 0;
+}
